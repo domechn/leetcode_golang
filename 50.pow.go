3832a7c1f820af2dbcdc1f4c4ca869824ac965f0
domgoer <814172254@qq.com>
2019-03-11 17:54:24 +0800

    50

---
+++ 50.pow.go
@@ -0,0 +1,26 @@
+/* ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : domchan
+#   Email         : 814172254@qq.com
+#   File Name     : 50.pow.go
+#   Created       : 2019-03-11 17:54:08
+#   Last Modified : 2019-03-11 17:54:08
+#   Describe      :
+#
+# ====================================================*/
+package leet
+
+func myPow(x float64, n int) float64 {
+	if n == 1 {
+		return x
+	}
+	if n == -1 {
+		return 1 / x
+	}
+	if n == 0 {
+		return 1.0
+	}
+	half := myPow(x, n/2)
+	return half * half * myPow(x, n%2)
+}
